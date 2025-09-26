e38fee5c9fda310c0d7366866a5e3cebb8faa4d6
Mahdi Kiani <mahdikiany@gmail.com>
2025-09-26 04:29:13 +0000

    Refactor server.py by removing unused logging and lifespan context manager
    - Eliminated the logging imports and the async lifespan context manager from server.py.
    - Streamlined the router inclusion for enrollment and usage routes.

--- app/server/server.py
+++ app/server/server.py
@@ -1,8 +1,4 @@
-import logging
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
-
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter
 from fastapi_mongo_base.core import app_factory
 
 from apps.enrollment.routes import router as enrollment_router
@@ -10,25 +6,12 @@ from apps.usage.routes import router as usage_router
 
 from . import config
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None]:  # noqa: RUF029
-    """Initialize application services."""
-    # await db.init_mongo_db()
-    logging.info("Startup complete")
-    yield
-    logging.info("Shutdown complete")
-
-
 app = app_factory.create_app(
     settings=config.Settings()  # ,  lifespan_func=lifespan,
 )
 server_router = APIRouter()
 
-for router in [
-    enrollment_router,
-    usage_router,
-]:
+for router in [enrollment_router, usage_router]:
     server_router.include_router(router)
 
 app.include_router(server_router, prefix=config.Settings.base_path)
